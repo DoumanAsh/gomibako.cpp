93af3a59b7b6e63a53b1a6aefa1ed85c947367fc
Douman <douman@gmx.se>
2017-08-16 14:33:07 +0300

    Properly close connection and accept new one with clear buffers

--- src/http/server.cpp
+++ src/http/server.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <optional>
 
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
@@ -22,47 +23,57 @@ class HttpListener {
         //Stores read data
         boost::beast::flat_static_buffer<8192> int_buffer;
 
-        boost::beast::http::request<boost::beast::http::dynamic_body> request;
-        boost::beast::http::response<boost::beast::http::dynamic_body> response;
+        //As we need to keep request/response alive as long  as async ops are in place
+        //we store them  inside of listener which is supposed to be alive
+        //We can use optional to quickly clear stuff using optional->emplace()
+        //TODO: Think about serialization http://www.boost.org/doc/libs/develop/libs/beast/doc/html/beast/using_http/serializer_stream_operations.html
+        std::optional<http::request<http::dynamic_body>> request;
+        std::optional<http::response<http::dynamic_body>> response;
 
     public:
         HttpListener(tcp::acceptor& acceptor) :
-            acceptor(acceptor),
-            socket(acceptor.get_io_service())
+            acceptor(acceptor)
         {
             this->accept();
         }
 
         /**
          * Handle new incoming connection.
+         *
+         * After finishing handling each new connection the function is called once again.
+         * The function should be called only after request's handling is finished!
+         * As long as it is called there will be jobs to do for boost::io_service and it will loop through them forever.
          */
         void accept() {
             this->acceptor.async_accept(this->socket, [this](boost::system::error_code error) {
                 if (error) {
                     utils::log_error(error, "Cannot accept incoming TCP");
+                    this->accept();
                 }
                 else {
                     this->handle_request();
                 }
-
-                //We continue accepting further requests to not leave
-                //us without work.
-                this->accept();
             });
         }
 
         /**
          * Handles HTTP Request.
+         *
+         * TODO: Next step is to think of API to provide routing.
          */
         void handle_request() {
-            this->response.set(http::field::content_type, "text/html");
-            boost::beast::ostream(this->response.body) << "<h1>Hello world</h1>\n";
+            this->response.emplace();
 
-            this->response.set(http::field::content_length, this->response.body.size());
-            http::async_write(this->socket, this->response,
+            this->response->set(http::field::content_type, "text/html");
+            boost::beast::ostream(this->response->body) << "<h1>Hello world</h1>\n";
+
+            this->response->set(http::field::content_length, this->response->body.size());
+            http::async_write(this->socket, *this->response,
             [this](boost::system::error_code error)
             {
                 this->socket.shutdown(tcp::socket::shutdown_send, error);
+                this->socket.close(error);
+                this->accept();
             });
 
         }
@@ -74,6 +85,7 @@ void Server::start() {
     boost::asio::io_service ios(this->config.cpu_num);
     tcp::acceptor acceptor{ios, {this->config.address, this->config.port}};
 
+    //Consider running few more listeners?
     HttpListener listener(acceptor);
 
     ios.run();
