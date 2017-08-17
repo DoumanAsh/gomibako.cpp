98284aae02411d6dd0fc8be784f6e97cff71384e
Douman <douman@gmx.se>
2017-08-17 14:55:42 +0300

    Add tutorial like commentary

--- src/http/server.cpp
+++ src/http/server.cpp
@@ -16,16 +16,43 @@ namespace http {
 using namespace http;
 using tcp = boost::asio::ip::tcp;
 
+/**
+ * HTTP boost::io_service handler.
+ *
+ * Object of this class should live as long as boost::io_service::run() is executing.
+ * It can be achieved through different means:
+ * * Use shared_ptr to control lifetime and refer to itself from each async closure.
+ * * Just create object on stack and let it live long enough (Maybe a bad idea to put it in heap in multithreaded environment).
+ *
+ * First can case can be found in multiple examples.
+ * But the second works as charm even in multithreaded environment.
+ * So why settle for unnecessary overhead of shared_ptr?
+ *
+ * TODO: There is a question whether you'd need a synchronization for pure heap-allocated members.
+ */
 class HttpListener {
     private:
+        /**
+         * Let us to accept incoming TCP connections.
+         *
+         * It can be shared across multiple instances of @ref HttpListener.
+         * Acceptor should be already aware of our IP and port.
+         */
         tcp::acceptor& acceptor;
+
+        /**
+         * Creates socket from reference to boost::io_service
+         *
+         * Socket is not yet opened at this moment.
+         * Only after performing first accept it will be actually opened.
+         */
         tcp::socket socket{acceptor.get_io_service()};
 
         ///5kb for reading incoming data
         ///Allocated on stack
         using static_buffer_type = boost::beast::flat_static_buffer<5120>;
 
-        //Stores read data
+        //Reading buffer for our async_read
         static_buffer_type read_buffer;
 
         //As we need to keep request/response alive as long  as async ops are in place
@@ -36,6 +63,9 @@ class HttpListener {
         std::optional<http::response<http::dynamic_body>> response;
 
     public:
+        //We don't really have any use without acceptor.
+        HttpListener() = delete;
+
         HttpListener(tcp::acceptor& acceptor) :
             acceptor(acceptor)
         {
@@ -92,6 +122,11 @@ class HttpListener {
             this->send_response();
         }
 
+        /**
+         * Reads Client's request
+         *
+         * On failure to read it would make sense to send back some meaningful error response.
+         */
         void read_request() {
             this->request.emplace();
 
@@ -130,7 +165,13 @@ class HttpListener {
 Server::Server(config::Config&& config) noexcept : config(std::move(config)) {}
 
 void Server::start() {
+    //Creates io_service and hints number of threads that can be used.
+    //As it is thread safe you don't need to worry about running the same service in multiple threads.
+    //Each thread that invokes io_service::run() will participate in distribution of incoming request.
+    //Note that you can use io_service::strand to ensure that handlers are not going to run concurrently.
     boost::asio::io_service ios(this->config.cpu_num);
+    //We create acceptor with tcp::endpoint by providing address and port.
+    //By default reuse of address it true, so we can have multiple acceptors.
     tcp::acceptor acceptor{ios, {this->config.address, this->config.port}};
 
     //Consider running few more listeners?
